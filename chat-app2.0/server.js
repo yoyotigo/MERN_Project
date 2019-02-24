7a974d89d6c479bd8e707575260edfda7cb51598
NickDiDomenico <DiDomenico@users.noreply.github.com>
2019-02-24 16:50:18 -0500

    socket work

--- chat-app2.0/server.js
+++ chat-app2.0/server.js
@@ -1,13 +1,22 @@
 var express = require("express")
 var mongoose = require("mongoose")
 var bodyParser = require("body-parser")
-
 var app = express()
 var http = require("http").Server(app)
 var io = require("socket.io")(http)
+var fs = require('fs');
+var options = {stream: fs.createWriteStream('events.log',{flags:'a'}) };
+var logger = require('socket.io-logger')(options);
+io.use(logger);
+
+
+
+
+
+
+
 
 var conString = "mongodb://admin:password123@ds149365.mlab.com:49365/chat-app"
-//var conString = "mongodb://localhost:27017/mylearning";
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -16,7 +25,12 @@ mongoose.Promise = Promise
 
 var Chats = mongoose.model("Chats", {
     name: String,
-    chat: String
+    chat: String,
+    date: Date
+})
+var Sockets = mongoose.model("Sockets",{
+    socket_id:String,
+    date:Date
 })
 
 mongoose.connect(conString, { useMongoClient: true }, (err) => {
@@ -31,6 +45,18 @@ app.post("/api/history", async (req, res) => {
         res.sendStatus(200)
         //Emit the event
         io.emit("chat", req.body)
+    } catch (error) {
+        res.sendStatus(500)
+        console.error(error)
+    }
+})
+app.post("/api/sockets", async (req, res) => {
+    try {
+        var sock = new Sockets(req.body)
+        await sock.save()
+        res.sendStatus(200)
+        //Emit the event
+        io.emit("sock",req.body)
     } catch (error) {
         res.sendStatus(500)
         console.error(error)
@@ -51,9 +77,15 @@ app.get("/api/history/roomname", (req, res) => {
     })
 })
 
-io.on("connection", (socket) => {
-    console.log("Socket is connected...")
-})
+////socket connections
+var clients = 0;
+io.on('connection', (socket)=> {
+console.log("Socket is connected...")
+   io.sockets.emit('broadcast',{ description: 'Welcome new user!'});
+   socket.on('disconnect', ()=> {
+      io.sockets.emit('broadcast',{ description: 'Goodbye User!'});
+   });
+});
 
 var server = http.listen(3020, () => {
     console.log("Well done, now I am listening on ", server.address().port)
