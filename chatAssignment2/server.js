86b8cc5955a599974c57b2630503486d256aa707
NickDiDomenico <DiDomenico@users.noreply.github.com>
2019-04-01 12:43:10 -0400

    routes/models/socket

--- chatAssignment2/server.js
+++ chatAssignment2/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 5000;
-var socket = require('socket.io');
+const express = require('express'),
+    app = express(),
+    path = require('path');
+    server = require('http').createServer(app),
+    io = require('socket.io')(server),
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser'),
+    port = process.env.PORT || 5000;
+    require('./socketManager/socket')(io);
 
 //////////PORT CONNECTION//////////
-server = app.listen(5000, function(){
-    console.log(`server is running on port 5000`)
-})
+server.listen(port, ()=>{
+    console.log('Listening on port ' + port);
+});
 
 //////////DATABASE CONNECTION//////////
 mongoose.connect('mongodb://admin:password123@ds349455.mlab.com:49455/testt',{ useMongoClient: true }, (err)=> {
@@ -17,7 +22,22 @@ mongoose.connect('mongodb://admin:password123@ds349455.mlab.com:49455/testt',{ u
     }
 })
 
+mongoose.Promise = global.Promise;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+
+
+/*app.use('/chats', require('./routes/chats'));
+app.use('/', require('./routes/index'));
+app.use(express.static(path.join(__dirname + '/assets'))); 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/assets/index.html'))
+    })
+*/
+
+
+/*
 io = socket(server);
 
 io.on('connection', (socket)=>{
@@ -32,3 +52,4 @@ io.on('connection', (socket)=>{
     })
 });
 
+*/
